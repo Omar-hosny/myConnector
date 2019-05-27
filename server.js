1b50464e2640aeb3b7f491b80a1ebd2104b0646d
omar hosny <omarhosnystar@gmail.com>
2019-05-27 09:25:44 -0700

    prepared for deploy

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+// load routes
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+// bodyParser middleWare
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// connect to DB
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("mongoDB connected.."))
+  .catch(err => console.log(err.message));
+
+// passport  middleware
+app.use(passport.initialize());
+
+// require config
+require("./config/passport")(passport);
+
+// use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
