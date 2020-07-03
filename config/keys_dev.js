834f5db432a06abd5905d6ca0b1ee16900943a82
omar hosny <omarhosnystar@gmail.com>
2020-07-03 13:30:00 +0200

    updated.. 212

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -3,7 +3,3 @@ module.exports = {
     "mongodb+srv://omar:omar1234@cluster0.rhqga.mongodb.net/<dbname>?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
-
-// / mongoURI: "mongodb://localhost/myDevconnector",
-
-// mongoURI: "mongodb://localhost/myDevconnector"
